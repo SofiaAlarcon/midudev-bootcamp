872bdfc7381635e2dddd3d0a9ac35667bfed837a
sofia <sofialarcon023@gmail.com>
2023-01-13 15:02:01 -0300

    punto

--- src/Mensaje.js
+++ src/Mensaje.js
@@ -2,7 +2,7 @@ const Mensaje = (props) => {
     return <h1 style = {{color: props.color}}>
       {props.message}
     </h1> 
-    // llama a la propiedad "message" del componente "Mensaje" (que es utilizado en el componente "App" en el archivo "App.js") y muestra su valor por pantalla
+    // llama a la propiedad "message" del componente "Mensaje" (que es utilizado en el componente "App" en el archivo "App.js") y muestra su valor por pantalla.
   }
 
   export default Mensaje
